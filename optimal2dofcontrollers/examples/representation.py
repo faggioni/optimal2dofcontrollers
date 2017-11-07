bedc15886f319df8c5bc228beae8b2bfc21cc070
Faggioni <miguelfaggioni@hotmail.com>
2017-11-07 17:53:30 -0400

    Trouble with VEnv

--- optimal2dofcontrollers/examples/representation.py
+++
@@ -1,6 +0,0 @@
-from optimal2dofcontrollers import representation as r
-import control as ctr
-import numpy as np
-
-system = ctr.tf([0.0169],[1,0.1312,0.0043])
-[A,B,C] = r.two_degrees_system(system)
